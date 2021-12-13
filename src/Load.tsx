f8937d70731275753fa613c38a35c85767763208
Griffin <Griffin04901@gmail.com>
2021-12-12 18:15:37 -0600

    drag and drop to re-order

--- src/Load.tsx
+++ src/Load.tsx
@@ -1,75 +1,122 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { BaseSyntheticEvent, useEffect, useState } from "react";
+import React, { BaseSyntheticEvent, useEffect, useState } from 'react';
 import IJumperObject from './IJumperObject';
 
-function Load(props: { id: string, number?: number, initialJumperList: IJumperObject[], removeLoad: (id: string) => void },  key: string) {
-    const [jumperList, setJumpers] = useState<IJumperObject[]>([]);
-    const LOCAL_STORAGE_KEY = 'load' + props.id + '.jumperList';
+function Load(
+	props: {
+		id: string;
+		number?: number;
+		initialJumperList: IJumperObject[];
+		removeLoad: (id: string) => void;
+	},
+	key: string
+) {
+	const [jumperList, setJumpers] = useState<IJumperObject[]>([]);
+	const LOCAL_STORAGE_KEY = 'load' + props.id + '.jumperList';
 
-    useEffect(() => {
-        const storedJSON: string = localStorage.getItem(LOCAL_STORAGE_KEY) || ''
-        if (storedJSON.length === 0) {
-            // this is the case when we create a new load
-            setJumpers(props.initialJumperList);
-        }
-        else {
-            setJumpers(JSON.parse(storedJSON));
-        }
-        
-    }, [])
+	useEffect(() => {
+		const storedJSON: string = localStorage.getItem(LOCAL_STORAGE_KEY) || '';
+		if (storedJSON.length === 0) {
+			// this is the case when we create a new load
+			setJumpers(props.initialJumperList);
+		} else {
+			setJumpers(JSON.parse(storedJSON));
+		}
+	}, []);
 
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(jumperList))
-    }, [jumperList]);
+	useEffect(() => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(jumperList));
+	}, [jumperList]);
 
-    function handleCancel(e: BaseSyntheticEvent) {
-        const id = e.target.offsetParent.firstChild.innerText;
-        // do this better with html dataset
-        setJumpers(jumperList.filter(entry => {
-            return entry.id !== id;
-        }));
-    }
+	function handleCancel(e: BaseSyntheticEvent) {
+		const id = e.target.offsetParent.firstChild.innerText;
+		// do this better with html dataset
+		setJumpers(
+			jumperList.filter((entry) => {
+				return entry.id !== id;
+			})
+		);
+	}
 
-    function handleOnDrop(event: React.DragEvent<HTMLDivElement>) {
-        event.preventDefault();
-        const newId=event.dataTransfer?.getData('text/id');
-        const newName=event.dataTransfer?.getData('text/name')
-        setJumpers(prevJumpers => {
-            return[...prevJumpers, {id: newId, name: newName}]
-        });
-        event.stopPropagation();
-    }
+	function loadContainsJumper(id: string) {
+		return (
+			jumperList.filter((entry) => {
+				return entry.id === id;
+			}).length > 0
+		);
+	}
 
-    function handleOnDragOver(event: React.DragEvent<HTMLDivElement>) {
-        event.preventDefault();
-    }
+	function handleOnDrop(event: React.DragEvent<HTMLDivElement>) {
+		event.preventDefault();
+		if (event.dataTransfer.types[0] === 'text/jumper') {
+			const data = JSON.parse(event.dataTransfer.getData('text/jumper'));
+			const newId = data.id;
+			const newName = data.name;
 
-    function handleLoadDelete() {
-        props.removeLoad(props.id)
-    }
+			if (jumperList.length >= 4) {
+				alert(`You cannot add ${newName} to the load, it is already full :(`);
+			} else if (newId !== '') {
+				if (!loadContainsJumper(newId)) {
+					setJumpers((prevJumpers) => {
+						return [...prevJumpers, { id: newId, name: newName }];
+					});
+				} else {
+					alert(`${newName} is already on this load!`);
+				}
+			}
+			event.stopPropagation();
+		}
+	}
 
-    return (
-        <div className="load card" 
-            onDrop={handleOnDrop} 
-            onDragOver={handleOnDragOver}>
-            <div className="loadId" >
-                {props.number?.toString()}
-            </div>
-            
-            <div className="jumperList">
-                <ul> 
-               {jumperList === undefined ? '' : jumperList.map(jumper => { // TODO: create a new component
-                   return (<span  className='jumper card'  key={jumper.id}>
-                       <div className="hide">{jumper.id}</div>
-                       <li>{jumper.name}</li>
-                       <div className="cancel" onClick={handleCancel}>X</div>
-                   </span>)
-               })}
-               </ul>
-            </div>
-            <button onClick={handleLoadDelete}>Delete</button>
-        </div>
-    )
+	function handleOnDragOver(event: React.DragEvent<HTMLDivElement>) {
+		event.preventDefault();
+		if (event.dataTransfer.types[0] !== 'text/jumper') {
+			// don't allow dropping an existing load onto another
+			//event.dataTransfer.dropEffect = 'none';
+		}
+	}
+
+	function handleLoadDelete() {
+		props.removeLoad(props.id);
+	}
+
+	return (
+		<div
+			className="load card"
+			onDrop={handleOnDrop}
+			onDragOver={handleOnDragOver}
+			draggable={true}
+			onDragStart={(event) => {
+				event.dataTransfer.setData('text/load', props.id);
+				if (props.number !== undefined) {
+					event.dataTransfer.setData('text/loadNum', props.number.toString());
+				}
+				
+			}}
+		>
+			<div className="loadId">{props.number?.toString()}</div>
+
+			<div className="jumperList">
+				<ul>
+					{jumperList === undefined
+						? ''
+						: jumperList.map((jumper) => {
+								// TODO: create a new component
+								return (
+									<span className="jumper card" key={jumper.id}>
+										<div className="hide">{jumper.id}</div>
+										<li>{jumper.name}</li>
+										<div className="cancel" onClick={handleCancel}>
+											X
+										</div>
+									</span>
+								);
+						  })}
+				</ul>
+			</div>
+			<button onClick={handleLoadDelete}>Delete</button>
+		</div>
+	);
 }
 
 export default Load;
--- src/LoadContainer.tsx
+++ src/LoadContainer.tsx
@@ -3,74 +3,168 @@ import Load from './Load';
 import { v4 as uuidv4 } from 'uuid';
 import IJumperObject from './IJumperObject';
 
-interface ILoadObject  {
-    id: string
-    number: number
-    jumperList: IJumperObject[]
-} 
+interface ILoadObject {
+	id: string;
+	number: number;
+	jumperList: IJumperObject[];
+}
 
 function LoadContainer() {
+	const [loadList, setLoadList] = useState<ILoadObject[]>([]);
+	const LOCAL_STORAGE_KEY = 'loadList';
+	const scrollToRef = useRef<HTMLDivElement>(null);
 
-    const [loadList, setLoadList] = useState<ILoadObject[]>([]);
-    const LOCAL_STORAGE_KEY = 'loadList';
-    const scrollToRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		const storedJSON: string = localStorage.getItem(LOCAL_STORAGE_KEY) || '';
+		if (storedJSON === '') return;
+		setLoadList(JSON.parse(storedJSON));
+	}, []);
 
-    useEffect(() => {
-        const storedJSON: string = localStorage.getItem(LOCAL_STORAGE_KEY) || ''
-        if (storedJSON === '') return;
-        setLoadList(JSON.parse(storedJSON));
-    }, [])
+	useEffect(() => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(loadList));
+	}, [loadList]);
 
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(loadList))
-    }, [loadList]);
+	function handleOnDrop(event: React.DragEvent<HTMLDivElement>) {
+		event.preventDefault();
+		if (event.dataTransfer.types[0] === 'text/jumper') {
+			const data = JSON.parse(event.dataTransfer.getData('text/jumper'));
+			const newId = data.id;
+			const newName = data.name;
+			if (newId !== '') {
+				setLoadList((previousLoads) => {
+					return [
+						...previousLoads,
+						{
+							id: uuidv4().toString(),
+							number: loadList.length + 1,
+							jumperList: [{ id: newId, name: newName }],
+						},
+					];
+				});
+			} else {
+			}
+			setTimeout(() => {
+				// have to have a short timeout to make this actually work
+				scrollToRef?.current?.scrollIntoView({
+					block: 'start',
+					behavior: 'smooth',
+				});
+			}, 100);
+		} else if (event.dataTransfer.types[0] === 'text/load') {
+			determinePosition(event);
+			reorderLoads(
+				event.dataTransfer.getData('text/load'),
+				determinePosition(event)
+			);
+		}
+	}
 
-    function handleOnDrop(event: React.DragEvent<HTMLDivElement>) {
-        event.preventDefault();
-        const newId=event.dataTransfer?.getData('text/id');
-        const newName=event.dataTransfer?.getData('text/name')
+	function getLoadPosition(id: string) {
+		for (let i = 0; i < loadList.length; i++) {
+			if (loadList[i].id === id) {
+				return i;
+			}
+		}
+		return 0;
+	}
 
-        setLoadList(previousLoads => {
-            return[...previousLoads, {id: uuidv4().toString(), number: loadList.length+1, jumperList: [{id: newId, name: newName}]}]
-        });
-        setTimeout(() => { // have to have a short timeout to make this actually work
-            scrollToRef?.current?.scrollIntoView({ block: 'start', behavior: 'smooth' });
-        }, 100);
-        
-    }
+	function reorderLoads(id: string, position: number) {
+		const newList: ILoadObject[] = [...loadList];
+		const index = getLoadPosition(id);
+		if (index + 1 < position) {
+			for (let i = position - 1; i >= index; i--) {
+				newList[i] = loadList[i + 1];
+			}
+			newList[position - 1] = loadList[index];
+		} else if (index + 1 > position) {
+			for (let i = 0; i <= position; i++) {
+				newList[i] = loadList[i > 0 ? i - 1 : 0];
+			}
+			newList[position - 1] = loadList[index];
+		}
+		let count = 1;
+		newList.forEach((load) => {
+			load.number = count;
+			count++;
+		});
+		setLoadList([...newList]);
+	}
 
-    function handleOnDragOver(event: React.DragEvent<HTMLDivElement>) {
-        event.preventDefault();
-    }
+	function determinePosition(event: React.DragEvent<HTMLDivElement>) {
+		const currentX = event.clientX;
+		const currentY = event.clientY;
+		const loads = Array.from(document.querySelectorAll('.load'));
+		const currentNumber = event.dataTransfer.getData('text/loadNum') || 0;
 
-    function removeLoad(id: string) {
-        let count = 0;
-        loadList.forEach(load => { // update the load count
-            if (load.id === id) return;
-            count++;
-            load.number = count;
-        })
-        setLoadList(loadList.filter(load => {
-            return load.id !== id;
-        }))
-        
-    }
+		let closest = loads.length - 1;
+		let minimumDistance = window.innerWidth;
+		for (let i = 0; i < loads.length; i++) {
+			const box = loads[i].getBoundingClientRect();
+			if (currentY > box.bottom || currentY < box.top) {
+				continue;
+			}
+			const middle = box.left + box.width / 2;
+			const distance = currentX - middle;
+			if (Math.abs(distance) < Math.abs(minimumDistance)) {
+				minimumDistance = distance;
+				closest = i;
+			}
+		}
+		let proposed = 0;
+		if (closest + 1 < currentNumber) {
+			proposed = minimumDistance < 0 ? closest + 1 : closest + 2;
+		} else {
+			proposed = minimumDistance > 0 ? closest + 1 : closest;
+		}
+		return proposed > 0 ? proposed : 1;
+	}
 
-    return (
-        <>
-            <div className='loadContainer' 
-                onDragOver={handleOnDragOver}
-                onDrop={handleOnDrop}>
-                
-                {loadList.map(load => {
-                    return (
-                        <Load id={load.id} number={load.number} key={load.id} initialJumperList={load.jumperList} removeLoad={removeLoad}/>
-                    )
-                })}
-                <div ref={scrollToRef} className="scrollTo"></div>
-            </div>
-        </>
-    )
+	function handleOnDragOver(event: React.DragEvent<HTMLDivElement>) {
+		event.preventDefault();
+		if (event.dataTransfer.types[0] !== 'text/load') {
+			return;
+		}
+	}
+
+	function removeLoad(id: string) {
+		let count = 0;
+
+		loadList.forEach((load) => {
+			// update the load count
+			if (load.id === id) return;
+			count++;
+			load.number = count;
+		});
+
+		setLoadList(
+			loadList.filter((load) => {
+				return load.id !== id;
+			})
+		);
+	}
+
+	return (
+		<>
+			<div
+				className="loadContainer"
+				onDragOver={handleOnDragOver}
+				onDrop={handleOnDrop}
+			>
+				{loadList.map((load) => {
+					return (
+						<Load
+							id={load.id}
+							number={load.number}
+							key={load.id}
+							initialJumperList={load.jumperList}
+							removeLoad={removeLoad}
+						/>
+					);
+				})}
+				<div ref={scrollToRef} className="scrollTo"></div>
+			</div>
+		</>
+	);
 }
 
 export default LoadContainer;
